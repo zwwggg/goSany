a8c5d756b824daf556283b25ec5b7f585b8ee782
zhaowei <657400766@qq.com>
2020-09-04 15:17:42 +0800

    1、添加版本信息，初始版本1.0.0 2、添加stcp连接方式，删除tcp连接方式 3、stcp中sk添加加密算法

--- frpconfig.go
+++ frpconfig.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"flag"
 	"fmt"
-	"gopkg.in/ini.v1"
+	"io"
 	"io/ioutil"
-	"os"
 	"strings"
+
+	"gopkg.in/ini.v1"
 )
 
 var frpcFilePath = "/etc/frp/frpc.ini"
 var cfg0FilePath = "/sys/fsl_otp/HW_OCOTP_CFG0"
 var cfg1FilePath = "/sys/fsl_otp/HW_OCOTP_CFG1"
+var version = "1.0.0"
 
 func getUid(filePath string) string {
 	cfg, err := ioutil.ReadFile(filePath)
@@ -20,8 +25,8 @@ func getUid(filePath string) string {
 	}
 
 	str := string(cfg)[2:]
-	str = strings.Replace(str, "\n", "",-1)
-	str = strings.Replace(str, "\r", "",-1)
+	str = strings.Replace(str, "\n", "", -1)
+	str = strings.Replace(str, "\r", "", -1)
 
 	for true {
 		if len(str) >= 8 {
@@ -34,45 +39,45 @@ func getUid(filePath string) string {
 }
 
 func fixFrpc(cfg *ini.File) {
+	uuid := ""
 	cfg.Section("common").Key("server_addr").SetValue("152.136.175.226")
 	cfg.Section("common").Key("server_port").SetValue("8000")
-	uid0, uid1 := getUid(cfg0FilePath), getUid(cfg1FilePath);
+	uid0, uid1 := getUid(cfg0FilePath), getUid(cfg1FilePath)
 	if uid0 != "" && uid1 != "" {
-		cfg.Section("common").Key("user").SetValue(uid0 + uid1)
+		uuid = uid0 + uid1
+		cfg.Section("common").Key("user").SetValue(uuid)
 	}
 	cfg.Section("common").Key("tls_enable").SetValue("true")
 	cfg.Section("common").Key("token").SetValue("sysd")
 
-	cfg.Section("ssh").Key("type").SetValue("tcp")
-	cfg.Section("ssh").Key("local_ip").SetValue("127.0.0.1")
-	cfg.Section("ssh").Key("local_port").SetValue("22")
-	cfg.Section("ssh").Key("remote_port").SetValue("0")
-	cfg.Section("ssh").Key("use_encryption").SetValue("true")
-	cfg.Section("ssh").Key("use_compression").SetValue("true")
+	cfg.Section("sssh").Key("type").SetValue("stcp")
+	cfg.Section("sssh").Key("sk").SetValue(getHmac("uuid", "sysd_sssh"))
+	cfg.Section("sssh").Key("local_ip").SetValue("127.0.0.1")
+	cfg.Section("sssh").Key("local_port").SetValue("22")
 
-	cfg.Section("codesys").Key("type").SetValue("tcp")
-	cfg.Section("codesys").Key("local_ip").SetValue("127.0.0.1")
-	cfg.Section("codesys").Key("local_port").SetValue("1217")
-	cfg.Section("codesys").Key("remote_port").SetValue("0")
-	cfg.Section("codesys").Key("use_encryption").SetValue("true")
-	cfg.Section("codesys").Key("use_compression").SetValue("true")
+	cfg.Section("sscodesys").Key("type").SetValue("stcp")
+	cfg.Section("sscodesys").Key("sk").SetValue(getHmac("uuid", "sysd_scodesys"))
+	cfg.Section("sscodesys").Key("local_ip").SetValue("127.0.0.1")
+	cfg.Section("sscodesys").Key("local_port").SetValue("1217")
 }
 
-func main() {
-	var cfg *ini.File
-	if _, err := os.Stat(frpcFilePath); err == nil { //文件存在
-		cfg, err = ini.Load(frpcFilePath)
+func getHmac(s string, k string) string {
+	h := hmac.New(sha256.New, []byte(k))
+	io.WriteString(h, s)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	} else if os.IsNotExist(err) { //文件不存在
-		cfg = ini.Empty()
-	} else { //未知错误
-		fmt.Println(err)
+var ver = flag.Bool("v", false, "version")
+
+func main() {
+	//返回软件版本
+	flag.Parse()
+	if *ver {
+		fmt.Println(version)
 		return
 	}
+
+	cfg := ini.Empty()
 
 	fixFrpc(cfg)
 	cfg.SaveTo(frpcFilePath)
